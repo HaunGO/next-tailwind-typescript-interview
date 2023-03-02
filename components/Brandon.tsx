5632ab50d3a0aef3371ed8878774b3d36e0ecfdd
Brandon Haun <haungo@gmail.com>
2023-03-02 00:55:11 -0500

    base upgrade commit
    added a few things: +brand  +layouts +thing

---
+++ components/Brandon.tsx
@@ -0,0 +1,8 @@
+export default function Brandon() {
+  return (
+    <aside id="brandon" className="">
+        <span id="bh-name">Brandon Haun</span>
+        <span id="bh-tag">Front End + Full Stack : Software Engineer</span>
+    </aside>
+  );
+}
---
+++ components/Thing.tsx
@@ -0,0 +1,7 @@
+export default function Thing() {
+  return (
+    <>
+      <code>Thing</code>
+    </>
+  );
+}
